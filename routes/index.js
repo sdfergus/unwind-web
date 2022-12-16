caa2dddda83adf6da8493f614fb01709b856580b
Shandira <shandira.ferguson@gmail.com>
2022-12-15 23:24:18 -0800

    Mods added to project

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,9 @@ const router = express.Router();
 router.get( '/', ( req, res ) => {
   res.render( 'index', { title: 'Unwind Web' } );
 } );
+
+router.get( '/login', ( req, res ) => {
+  res.render( 'login', { title: 'Login' } );
+} );
+
+module.exports = router;
