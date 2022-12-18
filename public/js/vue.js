44be5e3862af7f07a96ec8079755f4ef36ba9d32
Shandira <shandira.ferguson@gmail.com>
2022-12-17 23:07:02 -0800

    Minor adjustments

--- public/js/vue.js
+++ public/js/vue.js
@@ -1,87 +1,87 @@
 Vue.component( 'playlist-selection', {
   props: [ 'soundtrack' ],
   template:
-    // '<div class="soundtrack-row"><img :src="soundtrack.img"><h3>{{ soundtrack.name }}</h3><p>{{ soundtrack.type }}</p><button>Add to Cart</button></div>',
-
     '<a class="list-group-item list-group-item-action d-flex justify-content-between align-items-center" data-toggle="list" href="#home" role="tab"><span class="mr-auto"><i class="bi bi-heart mr-4"></i><img :src="soundtrack.img" alt="soundtrack.alt"><span class="ml-4">{{ soundtrack.name }}</span></span><span class="mr-5">{{ soundtrack.type }}</span></a>'
-} );
+}
+);
 
 //Vue instance
 let vm = new Vue( {
-  el: '#vue',
+  // el: '#pills-tabContent2',
+  el: '#pills-tabContent2',
 
   data: {
     soundtracks: [
       {
-        img: '/public/images/weather-2.jpg',
+        img: '/images/weather-2.jpg',
         alt: 'Weather image of clouds',
-        name: 'Pasta',
+        name: 'soundtrack_name',
         type: 'Weather',
       },
       {
-        img: '/public/images/weather-2.jpg',
+        img: '/images/weather-2.jpg',
         alt: 'Weather image of clouds',
-        name: 'Red Crab',
+        name: 'soundtrack_name',
         type: 'Weather',
       },
       {
-        img: '/public/images/weather-2.jpg',
+        img: '/images/weather-2.jpg',
         alt: 'Weather image of clouds',
-        name: 'Breakfast Special',
+        name: 'soundtrack_name',
         type: 'Weather',
       },
       {
-        img: '/public/images/weather-2.jpg',
+        img: '/images/weather-2.jpg',
         alt: 'Weather image of clouds',
-        name: 'Sea Shells Mix',
+        name: 'soundtrack_name',
         type: 'Weather',
       },
       {
-        img: '/public/images/weather-2.jpg',
+        img: '/images/weather-2.jpg',
         alt: 'Weather image of clouds',
-        name: 'Ultimate Kebab',
+        name: 'soundtrack_name',
         type: 'Weather',
       },
       {
-        img: '/public/images/weather-2.jpg',
+        img: '/images/weather-2.jpg',
         alt: 'Weather image of clouds',
-        name: 'Chicken Steak',
+        name: 'soundtrack_name',
         type: 'Weather',
       },
       {
-        img: '/public/images/weather-2.jpg',
+        img: '/images/weather-2.jpg',
         alt: 'Weather image of clouds',
-        name: 'Filet Mignon',
+        name: 'soundtrack_name',
         type: 'Weather',
       },
       {
-        img: '/public/images/weather-2.jpg',
+        img: '/images/weather-2.jpg',
         alt: 'Weather image of clouds',
-        name: 'Bistro Brunch',
+        name: 'soundtrack_name',
         type: 'Weather',
       },
       {
-        img: '/public/images/weather-2.jpg',
+        img: '/images/weather-2.jpg',
         alt: 'Weather image of clouds',
-        name: 'California Crepe',
+        name: 'soundtrack_name',
         type: 'Weather',
       },
       {
-        img: '/public/images/weather-2.jpg',
+        img: '/images/weather-2.jpg',
         alt: 'Weather image of clouds',
-        name: 'Top Burger',
+        name: 'soundtrack_name',
         type: 'Weather',
       },
       {
-        img: '/public/images/weather-2.jpg',
+        img: '/images/weather-2.jpg',
         alt: 'Weather image of clouds',
-        name: 'Chicken Garlic Pizza',
+        name: 'soundtrack_name',
         type: 'Weather',
       },
       {
-        img: '/public/images/weather-2.jpg',
+        img: '/images/weather-2.jpg',
         alt: 'Weather image of clouds',
-        name: 'Wok away healthy',
+        name: 'soundtrack_name',
         type: 'Weather',
       },
     ],
