0698d8472ee2cf47d8ee70011e317e61d73da76a
Shandira <shandira.ferguson@gmail.com>
2022-12-17 23:07:23 -0800

    Weather catalog integrated

--- routes/index.js
+++ routes/index.js
@@ -15,4 +15,8 @@ router.get( '/contact', ( req, res ) => {
   res.render( 'contact', { title: 'Contact Us' } );
 } );
 
+router.get( '/weather', ( req, res ) => {
+  res.render( 'weather', { title: 'Catalog/Weather' } );
+} );
+
 module.exports = router;
